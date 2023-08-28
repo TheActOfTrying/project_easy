fc4b5bfa1a289aa8cb146defd86887a7b4dae428
Celine Victoria <celine.victoria@hotmail.no>
2023-08-28 19:39:38 +0200

    New boolean isPdf parameter

--- lib/machine.dart
+++ lib/machine.dart
@@ -1,6 +1,7 @@
 class Machine {
   final String machineType;
   final List<String> errorCodes;
+  final bool isPdf;
 
-  const Machine(this.machineType, this.errorCodes);
+  const Machine(this.machineType, this.errorCodes, this.isPdf);
 }
