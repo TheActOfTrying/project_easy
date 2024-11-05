f2bacaf84cf37b93a27a9c11e4d63e706728f010
The Act Of Trying <118830983+TheActOfTrying@users.noreply.github.com>
2024-11-05 17:02:57 +0100

    Update home_page.dart
    Sync_5_11_2024

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<MyHomePage> {
             padding: EdgeInsets.all(8),
             child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
               Text(
-                'Version 1.0.9',
+                'Version 1.0.9 Main',
                 style: TextStyle(color: Colors.grey),
               )
             ]),
